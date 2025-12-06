69d0a8d255edbde0941b2f5badd51a62ff94d144
mariajosevillagrazapata-ship-it <mariajosevillagrazapata@gmail.com>
2025-12-05 21:45:34 -0300

    vincular imagen

---
+++ ejercicio 02/assets /js/promedio.js
@@ -0,0 +1,7 @@
+let nota1 = parseFloat(prompt('Ingrese la primera nota 1'));
+let nota2 = parseFloat(prompt('Ingrese la segunda nota 2'));
+let nota3 = parseFloat(prompt('Ingrese la tercera nota 3'));
+
+let promedio = (nota1 + nota2 + nota3) / 3;
+
+alert(promedio);
